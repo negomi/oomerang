0a2001019f2805a81ae5157c04d29b270de91bf0
Imogen <imogen.wentworth@gmail.com>
2013-11-17 21:26:36 -0800

    Tidy up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,12 @@ Oomerang::Application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
 
+  resources :users, :items, :locations
+
   root to: "users#index"
   post "/items/found" => "items#found"
   post "/items/lost" => "items#lost"
   get "/users/logged" => "users#isLogged"
-
-  resources :users, :items, :locations
-
-  
-
   get "/mailertest" => "users#mailertest"
 
 end
